1b51dc8647ae7fcdae908bff4781a93c4c9d9c97
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-14 17:24:31 +0100

    3.5-Modifie Player et ajoute GameSession
    La classe `Player` est maintenant `internal` et toutes ses propriétés sont publiques, permettant un accès simplifié depuis d'autres classes.
    
    Ajout d'un nouvel espace de noms `Engine.ViewModels` et d'une classe `GameSession` qui gère un joueur courant avec des valeurs par défaut pour `Name`, `CharacterClass` et `Gold`.

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,17 @@
+﻿namespace Engine.ViewModels;
+using Engine.Models;
+
+class GameSession
+{
+    Player CurrentPlayer { get; set; }
+
+    GameSession()
+    {
+        CurrentPlayer = new Player
+        {
+            Name = "Tylux",
+            CharacterClass = "Fighter",
+            Gold = 1000000
+        };
+    }
+}
