1a8b094d4dfe99b406fe6091f82afdde16fc4211
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-15 19:26:07 +0100

    Amélioration de l'initialisation des objets de jeu
    Modification de `ItemFactory.cs` pour initialiser `_standardGameItems` avec un tableau d'objets `Weapon` et `QuestItem`, simplifiant ainsi le code. Mise à jour de la méthode `CreateGameItem` pour gérer le clonage des éléments et retourner `null` si aucun élément n'est trouvé.
    
    Ajout de la classe `QuestItem` dans `QuestItem.cs`, héritant de `GameItem`, avec un constructeur et une méthode `Clone` pour enrichir le système d'objets du jeu.

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -1,22 +1,27 @@
 ﻿using Engine.Models;
-namespace Engine.Factories;
-
-public static class ItemFactory
+namespace Engine.Factories
 {
-    private static List<GameItem> _standardGameItems;
-    static ItemFactory()
+    public static class ItemFactory
     {
-        _standardGameItems = new List<GameItem>();
-        _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
-        _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
-    }
-    public static GameItem CreateGameItem(int itemTypeID)
-    {
-        GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
-        if (standardItem != null)
+        private static readonly List<GameItem> _standardGameItems;
+        static ItemFactory()
         {
-            return standardItem.Clone();
+            _standardGameItems =
+            [
+                new Weapon(1001, "Pointy Stick", 1, 1, 2),
+                new Weapon(1002, "Rusty Sword", 5, 1, 3),
+                new QuestItem(9001, "Snake fang", 1),
+                new QuestItem(9002, "Snakeskin", 2),
+            ];
         }
-        return null;
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+            if (standardItem != null)
+            {
+                return standardItem.Clone();
+            }
+            return null;
+        }
     }
 }
---
+++ Engine/Models/QuestItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Engine.Models;
+
+public class QuestItem(int itemTypeID, string name, int price) : GameItem(itemTypeID, name, price)
+{
+    public override QuestItem Clone()
+    {
+        return new QuestItem(ItemTypeID, Name, Price);
+    }
+}
