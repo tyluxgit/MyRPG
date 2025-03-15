1dcea0715b0e482a8bf0bf050985c49646488e20
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-15 15:08:49 +0100

    Ajout de la navigation et des propriétés de localisation
    - Intégration de `PropertyChanged` et de `INotifyPropertyChanged` dans `GameSession` pour la notification des changements de propriété.
    - Changement de la position initiale de `CurrentLocation` à `(0, 0)`.
    - Ajout de propriétés pour vérifier les emplacements adjacents (nord, est, sud, ouest).
    - Implémentation de méthodes de déplacement (`MoveNorth`, `MoveEast`, `MoveSouth`, `MoveWest`) dans `GameSession`.
    - Mise à jour de `MainWindow.xaml` pour définir le `DataContext` sur `GameSession` et ajout d'un convertisseur `BooleanToVisibility`.
    - Remplacement du bouton "Add XP" par des boutons de déplacement dans l'interface utilisateur.
    - Révision de la logique de clic de bouton pour des méthodes spécifiques de mouvement.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,7 +1,10 @@
 ﻿using Engine.Models;
 using Engine.Factories;
+using PropertyChanged;
 
 namespace Engine.ViewModels;
+
+[AddINotifyPropertyChangedInterface]
 public class GameSession
 {
     public World CurrentWorld { get; set; }
@@ -21,7 +24,52 @@ public class GameSession
         };
         WorldFactory factory = new();
         CurrentWorld = factory.CreateWorld();
-        CurrentLocation = CurrentWorld.LocationAt(0, -1);
+        CurrentLocation = CurrentWorld.LocationAt(0, 0);
     }
+    public bool HasLocationToNorth
+    {
+        get
+        {
+            return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+        }
+    }
+    public bool HasLocationToEast
+    {
+        get
+        {
+            return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+        }
+    }
+    public bool HasLocationToSouth
+    {
+        get
+        {
+            return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+        }
+    }
+    public bool HasLocationToWest
+    {
+        get
+        {
+            return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+        }
+    }
+    public void MoveNorth()
+    {
+        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+    }
+    public void MoveEast()
+    {
+        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+    }
+    public void MoveSouth()
+    {
+        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+    }
+    public void MoveWest()
+    {
+        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+    }
+
 }
 
