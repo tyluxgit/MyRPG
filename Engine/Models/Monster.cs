ecfc43f50a09f7bc3eb273e831dff4c0981b47e5
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-15 22:14:26 +0100

    Ajout de la classe Monster avec propriétés et inventaire
    Implémentation de l'interface INotifyPropertyChanged dans la classe Monster.
    Ajout d'un constructeur pour initialiser les propriétés : Name, ImageName, MaximumHitPoints, HitPoints, RewardExperiencePoints et RewardGold.
    Création d'une collection observable Inventory, initialisée comme une liste vide.

---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using PropertyChanged;
+
+namespace Engine.Models;
+[AddINotifyPropertyChangedInterface]
+public class Monster(string name, string imageName,
+    int maximumHitPoints, int hitPoints,
+    int rewardExperiencePoints, int rewardGold)
+{
+    public string Name { get; private set; } = name;
+    public string ImageName { get; set; } = string.Format("pack://application:,,,/Engine;component/Images/Monsters/{0}", imageName);
+    public int MaximumHitPoints { get; private set; } = maximumHitPoints;
+    public int HitPoints { get; private set; } = hitPoints;
+    public int RewardExperiencePoints { get; private set; } = rewardExperiencePoints;
+    public int RewardGold { get; private set; } = rewardGold;
+    public ObservableCollection<ItemQuantity> Inventory { get; set; } = [];
+}
