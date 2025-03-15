9723dfff136f0a54f5889d6ecb5d9645a0d60193
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-15 19:36:25 +0100

    Mise à jour du projet et amélioration de la gestion des éléments
    Modification du cadre cible dans `Engine.csproj` pour `net9.0-windows` et ajout de WPF. Inclusion de la référence au package `PropertyChanged.Fody` version `4.1.0`. Suppression des lignes d'images inutilisées.
    
    Dans `ItemFactory.cs`, adoption d'une syntaxe de liste moderne pour les éléments de jeu. Changement du type de `standardItem` en nullable (`GameItem?`) et ajout d'une exception `ArgumentException` pour une meilleure gestion des erreurs.

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -7,21 +7,24 @@ namespace Engine.Factories
         static ItemFactory()
         {
             _standardGameItems =
-            [
-                new Weapon(1001, "Pointy Stick", 1, 1, 2),
-                new Weapon(1002, "Rusty Sword", 5, 1, 3),
-                new QuestItem(9001, "Snake fang", 1),
-                new QuestItem(9002, "Snakeskin", 2),
-            ];
+                [
+                    new Weapon(1001, "Pointy Stick", 1, 1, 2),
+                    new Weapon(1002, "Rusty Sword", 5, 1, 3),
+                    new QuestItem(9001, "Snake fang", 1),
+                    new QuestItem(9002, "Snakeskin", 2),
+                ];
         }
         public static GameItem CreateGameItem(int itemTypeID)
         {
-            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+            GameItem? standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
             if (standardItem != null)
             {
                 return standardItem.Clone();
             }
-            return null;
+
+            throw new ArgumentException($"Aucun élément de type {itemTypeID} n'existe.", nameof(itemTypeID));
         }
+
     }
 }
