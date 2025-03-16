2cb7489acf79084c0235e5cfb6044880b66995ec
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-16 08:06:44 +0100

    Ajout de la classe MonsterEncounter
    Implémentation d'une nouvelle classe `MonsterEncounter` dans l'espace de noms `Engine.Models`. Cette classe inclut les propriétés `MonsterID` et `ChanceOfEncountering`, initialisées par le constructeur.

---
+++ Engine/Models/MonsterEncounter.cs
@@ -0,0 +1,7 @@
+﻿namespace Engine.Models;
+
+public class MonsterEncounter(int monsterID, int chanceOfEncountering)
+{
+    public int MonsterID { get; set; } = monsterID;
+    public int ChanceOfEncountering { get; set; } = chanceOfEncountering;
+}
